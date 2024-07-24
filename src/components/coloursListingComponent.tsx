11104914dfad66e4cbceade427209620f0bc0ffc
ram-meegada <ramumeegada1811@gmail.com>
2024-07-24 23:02:19 +0530

    colours listing api

--- src/components/coloursListingComponent.tsx
+++ src/components/coloursListingComponent.tsx
@@ -1,65 +1,95 @@
-import React, { useEffect, useState } from 'react'
-import SideBar from '../components/sidebar'
-import '../css/manageColours.css'
-import { toast, ToastContainer } from 'react-toastify'
-import { BASE_URL } from '../utils'
+import React, { useEffect, useState } from "react";
+import SideBar from "../components/sidebar";
+import "../css/manageColours.css";
+import { toast, ToastContainer } from "react-toastify";
+import { BASE_URL } from "../utils";
+import { useNavigate } from "react-router-dom";
 
 type Colour = {
-    id: number,
-    name: string
-}
+  id: number;
+  name: string;
+};
 
 const ColoursListingComponent = () => {
-    const [ colors, setColors ] = useState<Colour[]>([])
-    const api_token = localStorage.getItem("auth_token")
+  const [colors, setColors] = useState<Colour[]>([]);
+  const api_token = localStorage.getItem("auth_token");
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        const fetchData = async () => {
-            const api_hit = await fetch(`${BASE_URL}colours/`, {
-                method: "GET",
-                headers: {
-                    Authorization: `Bearer ${api_token}`,
-                    "Content-Type": "application/json",
-                  },
-            })
-            const response = await api_hit.json()
-            if (api_hit.ok) {
-                setColors(response.data)
-                console.log(response.data, typeof(response.data), '------colors-------');
-            }
-            else {
-                toast.error(response.message)
-            }
+  useEffect(() => {
+    try {
+      const fetchData = async () => {
+        const api_hit = await fetch(`${BASE_URL}colours/`, {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${api_token}`,
+            "Content-Type": "application/json",
+          },
+        });
+        const response = await api_hit.json();
+        if (api_hit.ok) {
+          setColors(response.data)
+        } else {
+          toast.error(response.message);
         }
-        fetchData();
-    }, [])
+      };
+      fetchData();
+    } catch (err) {
+      console.log(err);
+    }
+  }, []);
 
-    return (
-        <>
-            <a href='/add-color' className='add-button'>Add Color</a>
-            <div className='colours-table'>
-                <table>
-                    <thead>
-                        <tr>
-                            <th>S.no</th>
-                            <th>Name</th>
-                            <th>Action</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {colors.map((value, index) => (
-                            <tr key={index}>
-                                <td>{value.id}</td>
-                                <td>{value.name}</td>
-                                <td></td>
-                            </tr>
-                        ))}
-                    </tbody>
-                </table>
-            </div>
-            <ToastContainer />
-        </>
-    )
-}
+  const handleDelete = async (id: number) => {
+    const api_hit = await fetch(`${BASE_URL}delete-color/${id}/`, {
+      method: "DELETE",
+      headers: {
+        Authorization: `Bearer ${api_token}`,
+        "Content-Type": "application/json",
+      },
+    })
+    const response = await api_hit.json();
+    if (api_hit.ok) {
+      window.location.reload();
+      toast.success(response.message)
+    }
+    else {
+      toast.error("Something went wrong")
+    }
+  }
+
+  return (
+    <>
+      <a href="/add-color" className="add-button">
+        Add Color
+      </a>
+      <div className="colours-table">
+        <table>
+          <thead>
+            <tr>
+              <th>S.no</th>
+              <th>Name</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {colors.map((value, index) => (
+              <tr key={index}>
+                <td>{index + 1}</td>
+                <td>{value.name}</td>
+                <td style={{ width: "20%" }}>
+                  <ul className="colours-table-action">
+                    <li><img src="view_logo.jpg" alt="view" /></li>
+                    <li><img src="edit_logo.png" alt="edit" /></li>
+                    <li onClick={() => handleDelete(value.id)} className="colours-table-action-delete"><img src="delete_logo.png" alt="delete" /></li>
+                  </ul>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      <ToastContainer />
+    </>
+  );
+};
 
 export default ColoursListingComponent;
--- src/features/addColour.tsx
+++ src/features/addColour.tsx
@@ -1,18 +1,57 @@
-import React from 'react'
-import SideBar from '../components/sidebar';
-import '../css/addColor.css'
+import React, { useState } from "react";
+import SideBar from "../components/sidebar";
+import "../css/addColor.css";
+import { BASE_URL } from "../utils";
+import { useNavigate } from "react-router-dom";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const AddColour = () => {
-    return (
-        <>
-            <SideBar />
-            <div className='add-color-container'>
-                <p>Add Color</p>
-                <input type='text' placeholder='Enter color'></input>
-                <input type="submit" value="Submit" className="add-color-btn-submit"></input>
-            </div>
-        </>
-    )
-}
+  const [name, setName] = useState("");
+  const api_token = localStorage.getItem("auth_token");
+  const navigate = useNavigate();
+
+  const PAYLOAD = { name: name };
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    
+    const api_hit = await fetch(`${BASE_URL}add-color/`, {
+      method: "POST",
+      body: JSON.stringify(PAYLOAD),
+      headers: {
+        Authorization: `Bearer ${api_token}`,
+        "Content-Type": "application/json",
+      },
+    });
+    const response = await api_hit.json()
+    if (api_hit.ok) {
+        navigate("/home", { state: { toast_message: response.message } })
+    }
+    else {
+        toast.error(response.message)
+    }
+  };
+  return (
+    <>
+      <SideBar />
+      <form onSubmit={handleSubmit}>
+        <div className="add-color-container">
+          <p>Add Color</p>
+          <input
+            type="text"
+            placeholder="Enter color"
+            onChange={(e) => setName(e.target.value)}
+          ></input>
+          <input
+            type="submit"
+            value="Submit"
+            className="add-color-btn-submit"
+          ></input>
+        </div>
+      </form>
+      <ToastContainer />
+    </>
+  );
+};
 
 export default AddColour;
