395097a161ddedc08dc84eb4149443abf7ee4f32
ram-meegada <ramumeegada1811@gmail.com>
2024-08-06 17:52:38 +0530

    changes in chat

--- src/features/chat.tsx
+++ src/features/chat.tsx
@@ -1,19 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import SideBar from '../components/sidebar'
 import '../css/chat.css'
 
 const ChatModule = () => {
+    const [ message, setMessage ] = useState("")
     return (
         <>
             <SideBar></SideBar>
             <div className='chat-main-div'>
                 <div className='chat-text-div'>
-                    <input type='text'></input>
-                    <button>send</button>
+                    <input type='text' className='chat-text-area' onChange={(e) => setMessage(e.target.value)} ></input>
+                    <button className='chat-send-button'>send</button>
+                </div>
+                <div className='chat-flow-div'>
+                    { message }
                 </div>
             </div>
         </>
     )
 }
 
-export default ChatModule
+export default ChatModule;
