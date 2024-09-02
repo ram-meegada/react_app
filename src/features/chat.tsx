d3933f309e59a3792437940aba05a6c44bea6893
ram-meegada <ramumeegada1811@gmail.com>
2024-09-02 19:33:52 +0530

    chjbsad

--- src/features/chat.tsx
+++ src/features/chat.tsx
@@ -7,6 +7,7 @@ import { PAYLOAD } from "../paylaod";
 
 const ChatModule = () => {
     const [messages, setMessages] = useState<string[]>([]);
+    const [string_message, setStringMessage] = useState("");
     const [input_message, setInputMessage] = useState("");
     const [socket, setSocket] = useState<WebSocket | null>(null)
     const [displayedText, setDisplayedText] = useState<string>('');
@@ -15,15 +16,16 @@ const ChatModule = () => {
     const typingInterval = useRef<number | null>(null);
     const [profile_picture, setprofilePicture] = useState("")
     const [index, setIndex] = useState(0)
+    const api_token = localStorage.getItem("auth_token");
 
     useEffect(() => {
         const n = "abcdefghijklmnopqrstuvwxyz"
         let random_string = ''
-        for (let i=0; i<=10; i++){
+        for (let i = 0; i <= 10; i++) {
             let num = Math.floor(Math.random() * n.length)
             random_string += n[num]
         }
-        const ws = new WebSocket(`ws://127.0.0.1:8000/ws/chat/`);
+        const ws = new WebSocket(`wss://whizzodevapi.appgrowthcompany.com/file-summarization/`);
         setSocket(ws)
         const profile_picture = localStorage.getItem("profile_picture")
         if (profile_picture) {
@@ -36,45 +38,58 @@ const ChatModule = () => {
             toast.success("Connection established successfully.")
         });
         socket?.addEventListener("message", (event) => {
-            // console.log(event.data, typeof(event.data), '------');
             const data = JSON.parse(event.data)
-            if (data.signal == 1) {
+            console.log(data, '------');
+            // const res = data.data.response
+            // console.log(res, typeof(res), '----data---');
+            // const res1 = res.replaceAll("\n", "<br />")
+            // setMessages([data.data])
+            setStringMessage(data.data)
+            // setFullMessage(data.data.response)
+            // console.log(fullMessage, '-----messages----');
 
-                setMessages([...messages, data.data])
-                // console.log(data.data);
-            }
-            else if (data.signal == 0) {
-                setSuccessMessage("Summary generated successfully")
-                console.log("---------Summary generated successfully--------");
+            // if (data.signal == 1) {
+            //    console.log(data.data, '-----data.data------');
 
-            }
+            //    setMessages([...messages, data.response])
+            //     // console.log(data.data);
+            // }
+            // else if (data.signal == 0) {
+            //     setSuccessMessage("Summary generated successfully")
+            //     console.log("---------Summary generated successfully--------");
+
+            // }
             // setFullMessage(event.data);
         })
         socket?.addEventListener("close", (e) => {
+            socket?.close()
             console.log(e, "---------connection closed--------");
             // toast.error("Connection closed");
         })
-        // return () => {
-        //     socket?.close()
-        // }
-    })
+        return () => {
+            socket?.close()
+        }
+    }, [string_message])
 
     useEffect(() => {
-        if (success_message){
+        if (success_message) {
             toast.success(success_message)
         }
     }, [success_message])
 
     const handleMessage = () => {
         // const data = {
-        //     "topic": "kane williamson",
-        //     "words": 500,
-        //     "language": "arabic",
-        //     "region": "New zealand",
-        //     "tone": "Realistic",
-        //     "pronouns": "Third person"
+        //     "reduce_citation": true,
+        //     "description":"give precisely",
+        //     "topic": "Kane Williamson",
+        //     "page": 1,
+        //     "tone": "Formal",
+        //     "reference": "harvard"
         // }
-        socket?.send(JSON.stringify({hit: input_message}))
+        const data = {
+            "question": " what is majra?"
+        }
+        socket?.send(JSON.stringify(PAYLOAD))
         setMessages(prevMessages => [...prevMessages, input_message])
         console.log(messages, '----messages----');
 
@@ -82,18 +97,24 @@ const ChatModule = () => {
         chat_input.value = ""
     }
 
+    function write_word_by_word() {
+        for (let i=0; i<=messages.length; i++) {
+            <div>messages[i]</div>
+        }
+    }
+
     return (
         <>
             <SideBar></SideBar>
             <div className="chat-container">
                 <div className="messages">
-                    {messages.map((value, index) => (
+                    {/* {messages.map((value, index) => (
                         <div key={index} className="message-box">
-                            {/* <img src={profile_picture} alt="profile"></img> */}
-                            <div dangerouslySetInnerHTML={{__html: value}} />
+                            <img src={profile_picture} alt="profile"></img>
+                            <div dangerouslySetInnerHTML={{ __html: value }} />
                         </div>
-                    ))}
-                    {/* {fullMessage} */}
+                    ))} */}
+                    <div dangerouslySetInnerHTML={{ __html: string_message }} />
                 </div>
                 <div className="input-container">
                     <input
